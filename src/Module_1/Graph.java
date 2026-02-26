3422807862c41a3e5245eded44412c5130d496ca
cit-24-02-0046-Dileepa <cit-24-02-0046@sltc.ac.lk>
2026-02-26 18:31:16 +0530

    Implemented addLocation method in Graph

--- src/Module_1/Graph.java
+++ src/Module_1/Graph.java
@@ -1,5 +1,16 @@
 package Module_1;
 
+import java.util.*;
+
+/*
+ * Graph
+ * Represents roads using adjacency list
+ */
 public class Graph {
-    
-}
+
+    private Map<String, List<String>> adjList = new HashMap<>();
+
+    // Add location
+    public void addLocation(String location) {
+        adjList.putIfAbsent(location, new ArrayList<>());
+    }

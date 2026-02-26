420a9c6dd8734a0acbcb2a27155ff2d9819a304b
hyperpsycho1 <farhath.nawas26@gmail.com>
2026-02-27 02:17:58 +0530

    create PerformanceUtil.java class

--- src/Module_2/PerformanceUtil.java
+++ src/Module_2/PerformanceUtil.java
@@ -1,11 +1,4 @@
 package Module_2;
-
 public class PerformanceUtil {
 
-    public static long measure(Runnable task) {
-        long start = System.nanoTime();
-        task.run();
-        long end = System.nanoTime();
-        return end - start;
-    }
 }

589d0bff755d63cc1294a66e0b356789f3c956d8
cit-24-02-0046-Dileepa <cit-24-02-0046@sltc.ac.lk>
2026-02-26 18:26:44 +0530

    Implemented BST insert functionality

--- src/Module_1/LocationBST.java
+++ src/Module_1/LocationBST.java
@@ -1,5 +1,27 @@
 package Module_1;
 
+/*
+ * LocationBST
+ * Implements Binary Search Tree for locations
+ */
 public class LocationBST {
-    
-}
+
+    private LocationNode root;
+
+    // Insert location into BST
+    public void insert(String locationName) {
+        root = insertRec(root, locationName);
+    }
+
+    private LocationNode insertRec(LocationNode root, String locationName) {
+
+        if (root == null)
+            return new LocationNode(locationName);
+
+        if (locationName.compareTo(root.locationName) < 0)
+            root.left = insertRec(root.left, locationName);
+        else if (locationName.compareTo(root.locationName) > 0)
+            root.right = insertRec(root.right, locationName);
+
+        return root;
+    }

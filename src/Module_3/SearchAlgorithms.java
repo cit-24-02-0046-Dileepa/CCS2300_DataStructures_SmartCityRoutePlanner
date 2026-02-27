f79f2adacbbac8703a2a2f4b1ed27c88f738d597
Ruchira2341 <ruchiranimnaka@gmail.com>
2026-02-27 21:25:57 +0530

    implement Binary Search algorithm for Module 3

--- src/Module_3/SearchAlgorithms.java
+++ src/Module_3/SearchAlgorithms.java
@@ -1,1 +1,25 @@
 
+package Module_3;
+
+public class SearchAlgorithms {
+    
+    // Implements one searching algorithm (Binary Search) as required
+    public static int binarySearch(int[] arr, int target) {
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (arr[mid] == target) {
+                return mid;
+            }
+            if (arr[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return -1; // Target not found
+    }
+}

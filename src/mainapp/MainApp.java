2bf1273181c8e910f53c00f48e686f8db10fe594
Ruchira2341 <ruchiranimnaka@gmail.com>
2026-02-28 00:36:10 +0530

    final mainapp is done(module1,module2,module3)

--- src/mainapp/MainApp.java
+++ src/mainapp/MainApp.java
@@ -3,6 +3,7 @@ package mainapp;
 import Module_1.Graph;
 import Module_1.LocationBST;
 import Module_2.DataSorter;
+import Module_3.PerformanceAnalyzer;
 import java.util.Scanner;
 
 public class MainApp {
@@ -13,10 +14,11 @@ public class MainApp {
         int choice;
 
         do {
-            System.out.println("\n===== MAIN MENU =====");
+            System.out.println("\n===== FULL PROJECT MAIN MENU =====");
             System.out.println("1. Module 1: Smart City Route Planner");
             System.out.println("2. Module 2: Data Sorter");
-            System.out.println("0. Exit");
+            System.out.println("3. Module 3: Algorithm Performance Analyzer");
+            System.out.println("0. Exit Application");
             System.out.print("Enter choice: ");
 
             choice = sc.nextInt();
@@ -29,11 +31,14 @@ public class MainApp {
                 case 2:
                     DataSorter.start();
                     break;
+                case 3:
+                    PerformanceAnalyzer.start();
+                    break;
                 case 0:
-                    System.out.println("Exiting...");
+                    System.out.println("Exiting Application...");
                     break;
                 default:
-                    System.out.println("Invalid choice.");
+                    System.out.println("Invalid choice. Please try again.");
             }
         } while (choice != 0);
         sc.close();
@@ -42,12 +47,12 @@ public class MainApp {
     private static void runModule1(Scanner sc, LocationBST bst, Graph graph) {
         int c;
         do {
-            System.out.println("\n--- Module 1 Sub-Menu ---");
-            System.out.println("1. Add Location | 2. Add Road | 3. Show Sorted | 4. Show Graph | 5. BFS | 0. Back");
-            System.out.print("Choose: ");
+            System.out.println("\n--- ROUTE PLANNER ---");
+            System.out.println("1. Add Location | 2. Add Road | 3. Display Sorted | 4. Connections | 5. BFS | 0. Back");
+            System.out.print("Enter choice: ");
             c = sc.nextInt();
             sc.nextLine();
-            
+
             if (c == 1) {
                 System.out.print("Enter location: ");
                 String loc = sc.nextLine();
